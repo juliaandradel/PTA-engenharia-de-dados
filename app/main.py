1231d438577f6ac5122bff5695ecf1b47bc40995
vitorwar2 <jvab2@cin.ufpe.br>
2025-11-20 17:37:19 -0300

    tratamento da tabela e criação no endpoint rodando na api

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 import uvicorn
+from app.services.tratamento__vendedores import clean_sellers
 from app.routers import example_router
 
 app = FastAPI(
@@ -15,5 +16,13 @@ async def read_root():
 @app.get("/health", description="Verifica a saúde da API.")
 async def health_check():
     return {"status": "ok"}
+
+@app.get("/vendedores-tratados", description="Retorna base de vendedores tratada.")
+async def get_vendedores_tratados():
+    # Indico o caminho do arquivo CSV como parâmetro
+    caminho_csv = "/app/data/[Júlia] DataLake - vendedores.csv"
+    df_tratado = clean_sellers(caminho_csv)
+    # Retorno as primeiras 10 linhas para facilitar o teste e evitar sobrecarga
+    return df_tratado.head(10).to_dict(orient="records")
 
 app.include_router(example_router, prefix="/example", tags=["Example"])
